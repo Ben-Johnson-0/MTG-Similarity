874cfbf0273d36b020a19280fbc4c34c5d7887d2
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2025-01-01 18:47:23 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,8 +2,14 @@ import tkinter as tk
 
 # Creates the root window
 def create_main_window() -> tk.Tk:
-    pass
+    root = tk.Tk()
+    root.title("MTG Card Similarity")
+    root.geometry("1200x860")
+
+    return root
 
 
 if __name__ == "__main__":
     print("WIP")
+    root = create_main_window()
+    root.mainloop()
