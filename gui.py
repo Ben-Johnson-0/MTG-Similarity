1f53e490b13dc9d81dbc3a9018995fe12343169d
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2025-01-01 19:01:42 -0500

    Added function prototypes
    Scaffolding for needed function features

--- gui.py
+++ gui.py
@@ -8,6 +8,29 @@ def create_main_window() -> tk.Tk:
 
     return root
 
+# Search bar for navigating cards via card-name
+def populate_search_section():
+    pass
+
+# Display area for showing cards that the user pinned
+def populate_pinned_section():
+    pass
+
+# Displays multiple cards that are given to it as a list
+def populate_display_section(cards:list) -> None:
+    pass
+
+# Shows a card's name, image, oracle text, and a sub window of other cards within it's similarity group
+def display_all_card_info(card_json:dict):
+    pass
+
+# Given a card's json data, display it's image
+def display_card(card_json:dict):
+    pass
+
+# Retrieve the card's image from Scryfall
+def fetch_image(card_url:str):
+    pass
 
 if __name__ == "__main__":
     print("WIP")
