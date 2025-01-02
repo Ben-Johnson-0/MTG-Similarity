cfe3c8fc58417b7b795643873ff17dc414e32fba
Ben Johnson <143573847+Ben-Johnson-0@users.noreply.github.com>
2025-01-01 19:13:09 -0500

    GUI basic search section
    Some basic scaffolding for the search section of the GUI

--- gui.py
+++ gui.py
@@ -9,8 +9,15 @@ def create_main_window() -> tk.Tk:
     return root
 
 # Search bar for navigating cards via card-name
-def populate_search_section():
-    pass
+def populate_search_section(master, cards:list):
+    search_label = tk.Label(master, text = "Search:")
+    search_text = tk.StringVar()
+    entry_bar = tk.Entry(master, textvariable=search_text, width=32)
+    search_button = tk.Button(master, text="Search", command = lambda: search_cards(cards, search_text) )
+
+    search_label.pack()
+    entry_bar.pack()
+    search_button.pack()
 
 # Display area for showing cards that the user pinned
 def populate_pinned_section():
@@ -31,6 +38,11 @@ def display_card(card_json:dict):
 # Retrieve the card's image from Scryfall
 def fetch_image(card_url:str):
     pass
+
+# Simple search by name
+def search_cards(cards:list, search_pattern:str) -> list:
+    pass
+
 
 if __name__ == "__main__":
     print("WIP")
